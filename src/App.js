835136e5b2bafa1c2524de68802ad6317cdf797b
Ketile Pimenta dos Santos <ketile.santos@venturus.org.br>
2021-02-18 21:18:04 -0300

    State comments

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import './App.css';
 import Comentario from './components/Comentario'
+import React, { Component} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Meu primeiro app react</h1>
-      <Comentario name="João" email="joao@gmail.com" data={new Date('2020, 3, 19')}>
-        Olá, tudo bem?
-      </Comentario>
-      <Comentario name="Maria" email="maria@gmail.com" data={new Date('2020, 5, 09')}>
-        Olá, estou bem e você?
-      </Comentario>
-      
-    </div>
-  );
+class App extends Component {
+  state = {
+    comentarios: [
+      {name: 'João', email: 'joao@gmail.com', data:new Date('2020, 3, 19'), message: 'Olá, tudo bem?'},
+      {name: 'Pedro', email: 'pedro@gmail.com', data:new Date('2020, 3, 30'), message: 'Olá, estou bem e você?'}
+    ]
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <h1>Message Project</h1>
+
+        {this.state.comentarios.map(comentario =>(
+           <Comentario name={comentario.name} email={comentario.email} data={comentario.data}>
+            {comentario.message}
+           </Comentario>
+        ))}
+  
+      </div>
+    );
+  }
 }
 
 export default App;
